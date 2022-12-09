6bab398cd0dfe74061a0f9fae84bb30722bd5974
Samuel Varghese <gamingsam77@gmail.com>
2022-12-09 15:39:12 +0000

    created a roud robin tournament pairer, need tests

--- partners.py
+++ partners.py
@@ -13,7 +13,7 @@ class Person:
 # "Kelly", "Cailin", "Landi", "Tina", "Sophia", "Anita", "Gaberial",
 # "Estera", "Joshua", "Priti", "Samuel", "Chantelle", "Mirian", "Balveet"
 
-men_arr = ["Rasheed", "Yordi","Cailin", "Landi", "Gaberial", "Joshua", "Samuel"]
+men_arr = ["Rasheed", "Yordi","Cailin", "Landi", "Gaberial", "Joshua", "Samuel", "NULL"]
 women_arry = ["Aisha", "Kelly", "Tina", "Sophia", "Anita", "Estera", "Priti", "Chantelle", "Mirian", "Balveet"]
 
 possible_men_pairs = list(itertools.combinations(men_arr, 2))
@@ -22,8 +22,39 @@ possible_women_pairs = list(itertools.combinations(women_arry, 2))
 def pretty_print(list):
     return [print(item) for item in list]
 
-pair_permutation_men = list(itertools.permutations(possible_men_pairs))
-print(list(pair_permutation_men))
+def check_pair_possible(pair, person_already_paired_map):
+    for person in pair:
+        if person_already_paired_map.get(person) is True:
+            return False
+    return True
+
+def gen_all_uniqeue(pairs):
+    person_connected_map = {}
+    previous_pairs = {}
+    all_possible_pairs = []
+    possible_pairs = []
+    while True:
+        for pair in pairs:
+            pair = list(pair)
+            pair_string = ''.join(str(x) for x in pair)
+            if previous_pairs.get(pair_string) == True:
+                continue
+            # iterate throuh each individual in the pair and check if they already have a partner
+            if check_pair_possible(list(pair), person_connected_map):
+                possible_pairs.append(pair)
+                previous_pairs[pair_string   ] = True
+                for person in pair:
+                    person_connected_map[person] = True
+        if possible_pairs == []:
+            break
+        all_possible_pairs.append(possible_pairs)
+        person_connected_map = {}
+        possible_pairs = []
+        # break if no possible pairs
+        # if they don't then add to pairs_list
+    return all_possible_pairs
+
+print(gen_all_uniqeue(possible_men_pairs))
 print("----------------------------------")
 # pretty_print(possible_women_pairs)
 
