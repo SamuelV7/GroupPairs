fb8a32a033eaa52005904caab1e4a940f906807b
Samuel Varghese <gamingsam77@gmail.com>
2023-01-04 01:13:55 +0000

    still needs work

--- partners.py
+++ partners.py
@@ -13,7 +13,7 @@ class Person:
 # "Kelly", "Cailin", "Landi", "Tina", "Sophia", "Anita", "Gaberial",
 # "Estera", "Joshua", "Priti", "Samuel", "Chantelle", "Mirian", "Balveet"
 
-men_arr = ["Rasheed", "Yordi","Cailin", "Landi", "Gaberial", "Joshua", "Samuel", "NULL"]
+men_arr = ["Rasheed", "Yordi","Cailin", "Landi", "Gaberial", "Joshua", "Samuel"]
 women_arry = ["Aisha", "Kelly", "Tina", "Sophia", "Anita", "Estera", "Priti", "Chantelle", "Mirian", "Balveet"]
 
 possible_men_pairs = list(itertools.combinations(men_arr, 2))
@@ -22,41 +22,85 @@ possible_women_pairs = list(itertools.combinations(women_arry, 2))
 def pretty_print(list):
     return [print(item) for item in list]
 
-def check_pair_possible(pair, person_already_paired_map):
-    for person in pair:
-        if person_already_paired_map.get(person) is True:
-            return False
-    return True
+def circular_list(person_list, index):
+    if index == 0:
+        return 0
+    length = len(person_list)
+    return person_list[(index) % length]
 
-def gen_all_uniqeue(pairs):
-    person_connected_map = {}
-    previous_pairs = {}
-    all_possible_pairs = []
-    possible_pairs = []
-    while True:
-        for pair in pairs:
-            pair = list(pair)
-            pair_string = ''.join(str(x) for x in pair)
-            if previous_pairs.get(pair_string) == True:
-                continue
-            # iterate throuh each individual in the pair and check if they already have a partner
-            if check_pair_possible(list(pair), person_connected_map):
-                possible_pairs.append(pair)
-                previous_pairs[pair_string   ] = True
-                for person in pair:
-                    person_connected_map[person] = True
-        if possible_pairs == []:
-            break
-        all_possible_pairs.append(possible_pairs)
-        person_connected_map = {}
-        possible_pairs = []
-        # break if no possible pairs
-        # if they don't then add to pairs_list
-    return all_possible_pairs
+# circular_list(men_arr, men_arr[2])
 
-print(gen_all_uniqeue(possible_men_pairs))
+# output = circular_list(men_arr, 2)+ 9
+
+def gen_all_possible(the_list):
+    jumps = 1
+    person_claimed = {}
+    while jumps != len(the_list):
+        for i, person in enumerate(the_list):
+            if person_claimed.get(person) != True:
+                person_claimed[person] = True
+                jumped_location = i + jumps
+                persons_pair = circular_list(the_list, jumped_location)
+                if person_claimed.get(persons_pair) != True:
+                    person_claimed[persons_pair] = True
+                    print(f"{person} with {persons_pair} WEEK {jumps}")
+        print("----------------------------------")
+        person_claimed = {}
+        jumps += 1
+
+
+# gen_all_possible(men_arr)
 print("----------------------------------")
-# pretty_print(possible_women_pairs)
+# print(pretty_print(gen_all_uniqeue(possible_women_pairs)))
+
+
+# define the list of players
+players = ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', 'player7', 'player8']
+
+
+import random
+
+# define the list of players
+players = men_arr
+
+
+# shuffle the list of players
+random.shuffle(players)
+
+# shuffle the list of players
+results = []
+
+# loop through the rounds
+for round in range(len(players) - 1):
+  # create a list of matches for this round
+  round_matches = []
+  # if this is the first round, pair the top half with the bottom half
+  if round == 0:
+    # loop through the players in the top half of the list
+    for i in range(len(players) // 2):
+      # pair the top player with the bottom player
+      round_matches.append((players[i], players[len(players) - i - 1]))
+  # if this is the second round or later, pair the players within the top half
+  else:
+    # loop through the players in the top half of the list
+    for i in range(len(players) // 2):
+      # pair the current player with the next player
+      round_matches.append((players[i], players[i + 1]))
+  # add a bye for the remaining player
+  round_matches.append((players[len(players) // 2], None))
+  # add the matches to the results list
+  results.append(round_matches)
+  # rotate the list of players
+  players.insert(1, players.pop())
+
+# print the results
+print(results)
+
+
+# results = create_tournament(men_arr)
+
+# results_women = create_tournament(women_arry)
+# print the results
 
 
 
---
+++ round_robin.py
@@ -0,0 +1,5 @@
+import random
+
+men_arr = ["Rasheed", "Yordi", "Cailin", "Landi", "Gaberial", "Joshua", "Samuel"]
+women_arry = ["Aisha", "Kelly", "Tina", "Sophia", "Anita", "Estera", "Priti", "Chantelle", "Mirian", "Balveet"]
+
